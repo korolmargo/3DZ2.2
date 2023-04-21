a07e16557adc01811f3070577fe7f3b6873528da
«Margarita <korolmargo70@gmail.com>
2023-04-21 10:17:19 +0300

    «ДЗ2.2»

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int sumOnAccaunt = 100;
-        int addSum = 1000;
+        int addSum = 100;
 
-        if(addSum > 1000) {
-            int bonusSum = addSum/100;
+        if (addSum > 1000) {
+            int bonusSum = addSum / 100;
             System.out.println("Сумма бонусов " + bonusSum);
             int finalSumOnAccaunt = bonusSum + sumOnAccaunt + addSum;
             System.out.println("Сумма на балансе " + finalSumOnAccaunt);
@@ -14,11 +14,8 @@ public class Main {
 
             int finalSumOnAccaunt = sumOnAccaunt + addSum;
             System.out.println("Сумма на балансе " + finalSumOnAccaunt);
-            
+
         }
-
-
-
 
 
         System.out.println("Hello world!");
